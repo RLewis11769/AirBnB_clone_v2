b85e5a2b2846bac24059c1cb357bd741cdbc687a
Rachel Lewis <RLewis11769@gmail.com>
2021-06-13 21:38:30 +0000

    Fix pep8, notice that it's not executable

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # Fabric file that generates compressed .tgz archive
 # All .tgz files should be added to archive
-# Archive should have name web_static_<year><month><day><hour><minute><second>.tgz
+# Archive name: web_static_<year><month><day><hour><minute><second>.tgz
 # Point is to always have history of changes with datetime in name
 
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """ Pack up web_static directory """
@@ -13,7 +14,7 @@ def do_pack():
     try:
         now = datetime.now()
         # Create web_static_datetime.tgz file name
-        # .tgz extension is for compressed archive of files created by "tar" command
+        # .tgz extension for compressed archive created by "tar" command
         tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
         # Create path as ArchiveName in versions folder
         tarArchivePath = "versions/" + tarArchiveName
@@ -23,8 +24,8 @@ def do_pack():
         local("mkdir -p versions")
 
         # Uses fabric to create "tarball" aka compressed archive file
-        # -c creates new, -z filters through gzip, -v creates verbose listing, -f uses archive file
-        # Creates tarball in location listed in path using all files in web_static directory
+        # -c creates, -z filters through gzip, # -v is verbose, -f uses file
+        # Creates tarball in path using all files in web_static directory
         # Just memorize these options for creating
         local("tar -czvf " + tarArchivePath + " web_static")
 
