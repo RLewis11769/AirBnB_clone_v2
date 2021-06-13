d4c28cd1b7c152c384435771b8121e8fce238d30
Rachel Lewis <RLewis11769@gmail.com>
2021-06-13 21:33:49 +0000

    Add data to 1-pack

---
+++ 1-pack_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Fabric file that generates compressed .tgz archive
+# All .tgz files should be added to archive
+# Archive should have name web_static_<year><month><day><hour><minute><second>.tgz
+# Point is to always have history of changes with datetime in name
+
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """ Pack up web_static directory """
+    # Ensures successful operation every time
+    try:
+        now = datetime.now()
+        # Create web_static_datetime.tgz file name
+        # .tgz extension is for compressed archive of files created by "tar" command
+        tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        # Create path as ArchiveName in versions folder
+        tarArchivePath = "versions/" + tarArchiveName
+
+        # Uses fabric to make versions directory
+        # local command executes shell command locally
+        local("mkdir -p versions")
+
+        # Uses fabric to create "tarball" aka compressed archive file
+        # -c creates new, -z filters through gzip, -v creates verbose listing, -f uses archive file
+        # Creates tarball in location listed in path using all files in web_static directory
+        # Just memorize these options for creating
+        local("tar -czvf " + tarArchivePath + " web_static")
+
+        # Returns compressed archive in new folder
+        return tarArchivePath
+    except:
+        return None
