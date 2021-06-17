c6b560f85536f226ed2813b09ca88f05f1a228fc
Rachel Lewis <RLewis11769@gmail.com>
2021-06-17 23:33:19 +0000

    Check if function correct, not listening on port 5000

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+
+# Pass import_name/application package to Flask class
+# Tells Flask where to find resources (ie templates, static files)
+# Sets __name__ variable to module name (so depends on source file)
+app = Flask(__name__)
+
+
+# Determine which URL triggers function
+# Meaning output of function rendered on home page in this instance
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Displays "Hello HBNB!" """
+    # Return converted into response object with default parameters
+    return "Hello HBNB!"
+
+# Calls function when called
+# Shows errors if any
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
