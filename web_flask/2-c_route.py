4ed46ecb9173a9a77f8c6377aabe1ee42779e39c
Rachel Lewis <RLewis11769@gmail.com>
2021-06-18 13:03:21 +0000

    Not tested, but pushing progress

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Starts Flask web application on 0.0.0.0 port 5000
+Contains response for home page, /hbnb, and /c/<text>
+"""
+
+from flask import Flask
+
+
+# Pass import_name/application package to Flask class
+# Tells Flask where to find resources (ie templates, static files)
+# Sets __name__ variable to module name (so depends on source file)
+app = Flask(__name__)
+
+
+# Determine which URL triggers function
+# Meaning output of function rendered on home page in this instance
+@app.route('/', strict_slashes=False)
+def index():
+    """ Displays 'Hello HBNB!' """
+    # Return converted into response object with default parameters
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays 'HBNB' """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_plus_anything():
+    """ Displays C then text after /c/ """
+    return "C %s" % text
+
+# Calls function when called
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
